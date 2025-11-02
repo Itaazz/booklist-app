ad51672397b3818ac2c60e77b5acf06fd860ae9c
Itaazz <clement09167@gmail.com>
2025-11-02 14:37:24 +0100

    feat(ui): Last commit

--- service/config.ts
+++ service/config.ts
@@ -1,6 +1,6 @@
 import { Platform } from 'react-native';
 
-export const LOCAL_IP = '192.168.1.49';
+export const LOCAL_IP = 'localhost';
 
 export const BASE =
   Platform.OS === 'android' ? 'http://10.0.2.2:3000' : `http://${LOCAL_IP}:3000`;
